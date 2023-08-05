ce4deaad82e6afdf7fb5bd59234284d7e6bd91c3
ReFil <31960031+ReFil@users.noreply.github.com>
2023-08-05 19:47:17 +0100

    reewrite to ptp

--- app/drivers/sensor/gen4/gen4.c
+++ app/drivers/sensor/gen4/gen4.c
@@ -14,61 +14,24 @@ static int gen4_normal_read(const struct device *dev, uint8_t *buf, const uint8_
     return i2c_read_dt(&config->bus, buf, len);
 }
 
-static uint8_t gen4_checksum(uint8_t *buffer, uint8_t length) {
-    uint16_t temp;
-    uint8_t checksum = 0;
-    for (temp = 0; temp < length; temp++)
-        checksum += (*(buffer + temp) & 0xFF);
-    return checksum;
-}
-
-static int gen4_ext_read(const struct device *dev, const uint16_t addr, const uint8_t len,
-                         uint8_t *buffer) {
+static int gen4_i2c_init(const struct device *dev) {
     const struct gen4_config *cfg = dev->config;
-    uint8_t request[8] = {(uint8_t)(GEN4_EXT_ACCESS_READ & 0xFF),
-                          (uint8_t)((GEN4_EXT_ACCESS_READ >> 8) & 0xFF),
-                          (uint8_t)(addr & 0xFF),
-                          (uint8_t)((addr >> 8) & 0xFF),
-                          0,
-                          0,
-                          (uint8_t)(len & 0xFF),
-                          (uint8_t)((len >> 8) & 0xFF)};
-    int ret = i2c_write_read_dt(&cfg->bus, request, 8, buffer, (len + 3));
+    uint8_t request[2] = {0x20, 0x00};
+    uint8_t buffer[30];
+    int ret = i2c_write_read_dt(&cfg->bus, request, 2, buffer, 30);
     if (ret < 0) {
         LOG_ERR("ext read status: %d", ret);
         return ret;
     }
     LOG_DBG("received value %x", buffer[2]);
-    uint8_t checksum = gen4_checksum(buffer, len + 2);
-    if (checksum != *(buffer + len + 2)) {
-        LOG_ERR("checksum failed: %d", (int)checksum);
-        // return 1;
-    }
-    return 0;
-}
-
-static int gen4_ext_write_1byte(const struct device *dev, const uint16_t addr, const uint8_t val) {
-    const struct gen4_config *cfg = dev->config;
-
-    uint8_t sendbuf[10];
-
-    sendbuf[0] = (uint8_t)(GEN4_EXT_ACCESS_WRITE & 0xFF);
-    sendbuf[1] = (uint8_t)((GEN4_EXT_ACCESS_WRITE >> 8) & 0xFF);
-    sendbuf[2] = (uint8_t)(addr & 0xFF);
-    sendbuf[3] = (uint8_t)((addr >> 8) & 0xFF);
-    sendbuf[4] = 0;
-    sendbuf[5] = 0;
-    sendbuf[6] = (uint8_t)(1 & 0xFF);
-    sendbuf[7] = 0;
-    sendbuf[8] = val;
-    sendbuf[9] = gen4_checksum(sendbuf, 9);
-
-    int ret = i2c_write_dt(&cfg->bus, sendbuf, 10);
+    // enable absolute mode
+    uint8_t request2[10] = {0x05, 0x00, 0x34, 0x03, 0x06, 0x00, 0x04, 0x00, 0x04, 0x03};
+    ret = i2c_write_dt(&cfg->bus, request2, 10);
     if (ret < 0) {
         LOG_ERR("ext read status: %d", ret);
         return ret;
     }
-    return ret;
+    return 0;
 }
 
 static int gen4_channel_get(const struct device *dev, enum sensor_channel chan,
@@ -148,80 +111,42 @@ static int gen4_channel_get(const struct device *dev, enum sensor_channel chan,
 }
 
 static int gen4_sample_fetch(const struct device *dev, enum sensor_channel) {
-    uint8_t packet[52];
+    uint8_t packet[53];
     int ret;
     ret = gen4_normal_read(dev, packet, 52);
     if (ret < 0) {
         LOG_ERR("read status: %d", ret);
         return ret;
     }
-    if (!(packet[GEN4_REPORT_ID_SHIFT] == 9)) {
+    if (!(packet[REPORT_ID_SHIFT] == PTP_REPORT_ID)) {
         return -EAGAIN;
     }
-    struct gen4_data *data = dev->data;
-    data->btns = packet[GEN4_BTNS_SHIFT];
-    data->contacts = packet[GEN4_NUM_CONTACT_SHIFT];
-    // Finger 0
-    if (data->contacts & BIT(0)) {
-        data->fingers[0].present = packet[GEN4_F0_PALM_SHIFT] & BIT(1);
-        data->fingers[0].palm = packet[GEN4_F0_PALM_SHIFT] & BIT(7);
-        data->fingers[0].x =
-            (uint16_t)(packet[GEN4_F0_X_HI_SHIFT] << 8) + packet[GEN4_F0_X_LO_SHIFT];
-        data->fingers[0].y =
-            (uint16_t)(packet[GEN4_F0_Y_HI_SHIFT] << 8) + packet[GEN4_F0_Y_LO_SHIFT];
-        LOG_DBG("Finger 0 detected: %d", data->fingers[0].present);
-        LOG_DBG("Finger 0 palm: %d", data->fingers[0].palm);
-        LOG_DBG("Finger 0 x: %d", data->fingers[0].x);
-        LOG_DBG("Finger 0 y: %d", data->fingers[0].y);
 
+    uint16_t report_length = packet[LENGTH_LOWBYTE_SHIFT] | (packet[LENGTH_HIGHBYTE_SHIFT] << 8);
+
+    struct gen4_data *data = dev->data;
+
+    if (report_length == 12) {
+        data->contacts = packet[10];
+        data->btns = packet[11];
     } else {
-        data->fingers[0].present = false;
-        LOG_DBG("Finger 0 not detected: %d", data->fingers[0].present);
+        data->contacts = packet[11];
+        data->btns = packet[12];
     }
-    // Finger 1
-    if (data->contacts & BIT(1)) {
-        data->fingers[1].present = packet[GEN4_F1_PALM_SHIFT] & BIT(1);
-        data->fingers[1].palm = packet[GEN4_F1_PALM_SHIFT] & BIT(7);
-        data->fingers[1].x =
-            (uint16_t)(packet[GEN4_F1_X_HI_SHIFT] << 8) + packet[GEN4_F1_X_LO_SHIFT];
-        data->fingers[1].y =
-            (uint16_t)(packet[GEN4_F1_Y_HI_SHIFT] << 8) + packet[GEN4_F1_Y_LO_SHIFT];
-    } else {
-        data->fingers[1].present = false;
-    }
-    // Finger 2
-    if (data->contacts & BIT(2)) {
-        data->fingers[2].present = packet[GEN4_F2_PALM_SHIFT] & BIT(1);
-        data->fingers[2].palm = packet[GEN4_F2_PALM_SHIFT] & BIT(7);
-        data->fingers[2].x =
-            (uint16_t)(packet[GEN4_F2_X_HI_SHIFT] << 8) + packet[GEN4_F2_X_LO_SHIFT];
-        data->fingers[2].y =
-            (uint16_t)(packet[GEN4_F2_Y_HI_SHIFT] << 8) + packet[GEN4_F2_Y_LO_SHIFT];
-    } else {
-        data->fingers[2].present = false;
-    }
-    // Finger 3
-    if (data->contacts & BIT(3)) {
-        data->fingers[3].present = packet[GEN4_F3_PALM_SHIFT] & BIT(1);
-        data->fingers[3].palm = packet[GEN4_F3_PALM_SHIFT] & BIT(7);
-        data->fingers[3].x =
-            (uint16_t)(packet[GEN4_F3_X_HI_SHIFT] << 8) + packet[GEN4_F3_X_LO_SHIFT];
-        data->fingers[3].y =
-            (uint16_t)(packet[GEN4_F3_Y_HI_SHIFT] << 8) + packet[GEN4_F3_Y_LO_SHIFT];
-    } else {
-        data->fingers[3].present = false;
-    }
-    // Finger 4
-    if (data->contacts & BIT(4)) {
-        data->fingers[4].present = packet[GEN4_F4_PALM_SHIFT] & BIT(1);
-        data->fingers[4].palm = packet[GEN4_F4_PALM_SHIFT] & BIT(7);
-        data->fingers[4].x =
-            (uint16_t)(packet[GEN4_F4_X_HI_SHIFT] << 8) + packet[GEN4_F4_X_LO_SHIFT];
-        data->fingers[4].y =
-            (uint16_t)(packet[GEN4_F4_Y_HI_SHIFT] << 8) + packet[GEN4_F4_Y_LO_SHIFT];
-    } else {
-        data->fingers[4].present = false;
-    }
+
+    uint8_t finger_id = (packet[3] & 0xFC) >> 2;
+    LOG_DBG("FINGER ID: %d", finger_id);
+    // Finger data
+    data->fingers[finger_id].present = (packet[3] & 0x02) >> 1;
+    data->fingers[finger_id].palm = packet[3] & 0x01;
+    data->fingers[finger_id].x = (uint16_t)packet[4] | (uint16_t)(packet[5] << 8);
+    data->fingers[finger_id].y = (uint16_t)packet[6] | (uint16_t)(packet[7] << 8);
+
+    LOG_DBG("Finger detected: %d", data->fingers[finger_id].present);
+    LOG_DBG("Finger palm: %d", data->fingers[finger_id].palm);
+    LOG_DBG("Finger x: %d", data->fingers[finger_id].x);
+    LOG_DBG("Finger y: %d", data->fingers[finger_id].y);
+
     return 0;
 }
 
@@ -290,90 +215,13 @@ static int gen4_init(const struct device *dev) {
     struct gen4_data *data = dev->data;
     const struct gen4_config *config = dev->config;
 
-    LOG_WRN("gen4 start");
-    data->in_int = false;
-    int ret;
-    ret = gen4_ext_write_1byte(dev, GEN4_REG_RESETCONTROL, GEN4_RESET_CONTROL);
-    if (ret < 0) {
-        LOG_ERR("can't reset %d", ret);
-        return ret;
-    }
-    k_msleep(60);
-    uint8_t extcache[4] = {0};
-
-    ret = gen4_ext_read(dev, GEN4_REG_SECURITYSTATUS, 1, extcache); // Read and pritn chip ID
-    if (ret < 0) {
-        LOG_ERR("can't read chip id %d", ret);
-        return ret;
-    } else
-        LOG_DBG("Chip ID: %x", extcache[2]);
-
-    ret = gen4_ext_read(dev, GEN4_REG_FEEDCONFIG_1, 1, extcache); // Read feedconfig 1
-    if (ret < 0) {
-        LOG_ERR("can't reaf feedconfig 1 %d", ret);
-        return ret;
-    }
-    // Configure abs reporting
-    extcache[2] |= GEN4_PRI_FEED_EN;
-    extcache[2] |= GEN4_PRI_FEED_TYPE;
-    k_usleep(50);
-
-    ret = gen4_ext_write_1byte(dev, GEN4_REG_FEEDCONFIG_1, extcache[2]); // Write feedconfig 1
-    if (ret < 0) {
-        LOG_ERR("can't Write feedconfig 1 %d", ret);
-        return ret;
-    }
-    k_usleep(50);
-    ret = gen4_ext_read(dev, GEN4_REG_FEEDCONFIG_1, 1, extcache); // Read feedconfig 1
-    if (ret < 0) {
-        LOG_ERR("can't reaf feedconfig 1 %d", ret);
-        return ret;
-    }
-
-    /*if (config->no_taps || config->rotate_90) {
-        ret = gen4_ext_read(dev, GEN4_REG_FEEDCONFIG_2, 1, extcache); // Read feedconfig 2
-        if (ret < 0) {
-            LOG_ERR("can't reaf feedconfig 2 %d", ret);
-            return ret;
-        }
-        // Configure abs reporting
-        extcache[2] |= config->rotate_90 ? GEN4_SWAP_XY : 0;
-        extcache[2] |= config->no_taps ? GEN4_DISABLE_BUTTONS : 0;
-        k_usleep(50);
-
-        ret = gen4_ext_write_1byte(dev, GEN4_REG_FEEDCONFIG_2, extcache[2]); // Write feedconfig
-    2 if (ret < 0) { LOG_ERR("can't Write feedconfig 2 %d", ret); return ret;
-        }
-        k_usleep(50);
-    }
-    ret = gen4_ext_write_1byte(dev, GEN4_REG_ZIDLE, 1); // one Z-Idle packet
-    if (ret < 0) {
-        LOG_ERR("can't write %d", ret);
-        return ret;
-    }
-
-    ret = gen4_ext_read(dev, GEN4_REG_POWERCONTROL, 1, extcache); // Read Powercontrol
-    if (ret < 0) {
-        LOG_ERR("can't read Powercontrol %d", ret);
-        return ret;
-    }
-    // Configure deep sleep
-    extcache[2] |= config->sleep_en ? GEN4_DEEP_SLEEP_EN : 0;
-    k_usleep(50);
-
-    ret = gen4_ext_write_1byte(dev, GEN4_REG_POWERCONTROL, extcache[2]); // Write Powercontrol
-    if (ret < 0) {
-        LOG_ERR("can't Write Powercontrol %d", ret);
-        return ret;
-    }
-    k_usleep(50);
-    */
+    gen4_i2c_init(dev);
     gen4_sample_fetch(dev, 0);
 #ifdef CONFIG_GEN4_TRIGGER
     data->dev = dev;
     gpio_pin_configure_dt(&config->dr, GPIO_INPUT);
     gpio_init_callback(&data->gpio_cb, gen4_gpio_cb, BIT(config->dr.pin));
-    ret = gpio_add_callback(config->dr.port, &data->gpio_cb);
+    int ret = gpio_add_callback(config->dr.port, &data->gpio_cb);
     if (ret < 0) {
         LOG_ERR("Failed to set DR callback: %d", ret);
         return -EIO;
