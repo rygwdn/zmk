3998d4c555f21fc90170cf25a2eecbee34386c36
ReFil <31960031+ReFil@users.noreply.github.com>
2023-08-07 11:44:33 +0100

    feat(hid): Multi touch trackpad reporting

--- app/src/hog.c
+++ app/src/hog.c
@@ -47,13 +47,38 @@ enum {
 };
 
 static struct hids_report input = {
-    .id = 0x01,
+    .id = ZMK_REPORT_ID_KEYBOARD,
     .type = HIDS_INPUT,
 };
 
 static struct hids_report consumer_input = {
-    .id = 0x02,
+    .id = ZMK_REPORT_ID_CONSUMER,
     .type = HIDS_INPUT,
+};
+
+static struct hids_report trackpad_report = {
+    .id = ZMK_REPORT_ID_TRACKPAD,
+    .type = HIDS_INPUT,
+};
+
+static struct hids_report trackpad_capabilities = {
+    .id = ZMK_REPORT_ID_FEATURE_PTP_CAPABILITIES,
+    .type = HIDS_FEATURE,
+};
+
+static struct hids_report trackpad_hqa = {
+    .id = ZMK_REPORT_ID_FEATURE_PTPHQA,
+    .type = HIDS_FEATURE,
+};
+
+static struct hids_report trackpad_config = {
+    .id = ZMK_REPORT_ID_FEATURE_PTP_CONFIGURATION,
+    .type = HIDS_FEATURE,
+};
+
+static struct hids_report trackpad_selective = {
+    .id = ZMK_REPORT_ID_FEATURE_PTP_SELECTIVE,
+    .type = HIDS_FEATURE,
 };
 
 static bool host_requests_notification = false;
@@ -92,6 +117,51 @@ static ssize_t read_hids_consumer_input_report(struct bt_conn *conn,
     return bt_gatt_attr_read(conn, attr, buf, len, offset, report_body,
                              sizeof(struct zmk_hid_consumer_report_body));
 }
+#if IS_ENABLED(CONFIG_ZMK_TRACKPAD)
+static ssize_t read_hids_trackpad_input_report(struct bt_conn *conn,
+                                               const struct bt_gatt_attr *attr, void *buf,
+                                               uint16_t len, uint16_t offset) {
+    struct zmk_hid_ptp_report_body *report_body = &zmk_hid_get_ptp_report()->body;
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, report_body,
+                             sizeof(struct zmk_hid_ptp_report_body));
+}
+#endif
+// TODO
+/*
+static ssize_t read_hids_trackpad_capabilities_input_report(struct bt_conn *conn,
+                                                            const struct bt_gatt_attr *attr,
+                                                            void *buf, uint16_t len,
+                                                            uint16_t offset) {
+    struct zmk_hid_consumer_report_body *report_body = &zmk_hid_get_consumer_report()->body;
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, report_body,
+                             sizeof(struct zmk_hid_consumer_report_body));
+}
+
+static ssize_t read_hids_consumer_input_report(struct bt_conn *conn,
+                                               const struct bt_gatt_attr *attr, void *buf,
+                                               uint16_t len, uint16_t offset) {
+    struct zmk_hid_consumer_report_body *report_body = &zmk_hid_get_consumer_report()->body;
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, report_body,
+                             sizeof(struct zmk_hid_consumer_report_body));
+}
+
+static ssize_t read_hids_consumer_input_report(struct bt_conn *conn,
+                                               const struct bt_gatt_attr *attr, void *buf,
+                                               uint16_t len, uint16_t offset) {
+    struct zmk_hid_consumer_report_body *report_body = &zmk_hid_get_consumer_report()->body;
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, report_body,
+                             sizeof(struct zmk_hid_consumer_report_body));
+}
+
+static ssize_t read_hids_consumer_input_report(struct bt_conn *conn,
+                                               const struct bt_gatt_attr *attr, void *buf,
+                                               uint16_t len, uint16_t offset) {
+    struct zmk_hid_consumer_report_body *report_body = &zmk_hid_get_consumer_report()->body;
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, report_body,
+                             sizeof(struct zmk_hid_consumer_report_body));
+}
+*/
+// ENDTODO
 
 // static ssize_t write_proto_mode(struct bt_conn *conn,
 //                                 const struct bt_gatt_attr *attr,
@@ -139,6 +209,33 @@ BT_GATT_SERVICE_DEFINE(
     BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),
     BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,
                        NULL, &consumer_input),
+#if IS_ENABLED(CONFIG_ZMK_TRACKPAD)
+    BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT, BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,
+                           BT_GATT_PERM_READ_ENCRYPT, read_hids_trackpad_input_report, NULL, NULL),
+    BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),
+    BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,
+                       NULL, &trackpad_report),
+#endif /*                                                                                          \
+BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT, BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,               \
+      BT_GATT_PERM_READ_ENCRYPT, read_hids_consumer_input_report, NULL, NULL),                     \
+BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),            \
+BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,       \
+  NULL, &consumer_input),                                                                          \
+BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT, BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,               \
+      BT_GATT_PERM_READ_ENCRYPT, read_hids_consumer_input_report, NULL, NULL),                     \
+BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),            \
+BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,       \
+  NULL, &consumer_input),                                                                          \
+BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT, BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,               \
+      BT_GATT_PERM_READ_ENCRYPT, read_hids_consumer_input_report, NULL, NULL),                     \
+BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),            \
+BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,       \
+  NULL, &consumer_input),                                                                          \
+BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_REPORT, BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,               \
+      BT_GATT_PERM_READ_ENCRYPT, read_hids_consumer_input_report, NULL, NULL),                     \
+BT_GATT_CCC(input_ccc_changed, BT_GATT_PERM_READ_ENCRYPT | BT_GATT_PERM_WRITE_ENCRYPT),            \
+BT_GATT_DESCRIPTOR(BT_UUID_HIDS_REPORT_REF, BT_GATT_PERM_READ_ENCRYPT, read_hids_report_ref,       \
+  NULL, &consumer_input),*/
     BT_GATT_CHARACTERISTIC(BT_UUID_HIDS_CTRL_POINT, BT_GATT_CHRC_WRITE_WITHOUT_RESP,
                            BT_GATT_PERM_WRITE, NULL, write_ctrl_point, &ctrl_point));
 
@@ -260,6 +357,78 @@ int zmk_hog_send_consumer_report(struct zmk_hid_consumer_report_body *report) {
 
     return 0;
 };
+
+#if IS_ENABLED(CONFIG_ZMK_TRACKPAD)
+K_MSGQ_DEFINE(zmk_hog_ptp_msgq, sizeof(struct zmk_hid_ptp_report_body),
+              CONFIG_ZMK_BLE_PTP_REPORT_QUEUE_SIZE, 4);
+
+void send_ptp_report_callback(struct k_work *work) {
+    struct zmk_hid_ptp_report_body report;
+    while (k_msgq_get(&zmk_hog_ptp_msgq, &report, K_NO_WAIT) == 0) {
+        struct bt_conn *conn = destination_connection();
+        if (conn == NULL) {
+            return;
+        }
+
+        struct bt_gatt_notify_params notify_params = {
+            .attr = &hog_svc.attrs[13],
+            .data = &report,
+            .len = sizeof(report),
+        };
+
+        int err = bt_gatt_notify_cb(conn, &notify_params);
+        if (err) {
+            LOG_DBG("Error notifying %d", err);
+        }
+
+        bt_conn_unref(conn);
+    }
+};
+
+K_WORK_DEFINE(hog_ptp_work, send_ptp_report_callback);
+
+int zmk_hog_send_ptp_report(struct zmk_hid_ptp_report_body *report) {
+    int err = k_msgq_put(&zmk_hog_ptp_msgq, report, K_NO_WAIT);
+    if (err) {
+        switch (err) {
+        case -EAGAIN: {
+            LOG_WRN("PTP message queue full, dropping report");
+            return err;
+        }
+        default:
+            LOG_WRN("Failed to queue ptp report to send (%d)", err);
+            return err;
+        }
+    }
+
+    k_work_submit_to_queue(&hog_work_q, &hog_ptp_work);
+
+    return 0;
+};
+
+int zmk_hog_send_ptp_report_direct(struct zmk_hid_ptp_report_body *report) {
+    struct bt_conn *conn = destination_connection();
+    if (conn == NULL) {
+        return 1;
+    }
+
+    struct bt_gatt_notify_params notify_params = {
+        .attr = &hog_svc.attrs[13],
+        .data = report,
+        .len = sizeof(*report),
+    };
+
+    int err = bt_gatt_notify_cb(conn, &notify_params);
+    if (err) {
+        LOG_DBG("Error notifying %d", err);
+        return err;
+    }
+
+    bt_conn_unref(conn);
+
+    return 0;
+};
+#endif
 
 int zmk_hog_init(const struct device *_arg) {
     static const struct k_work_queue_config queue_config = {.name = "HID Over GATT Send Work"};
---
+++ app/src/trackpad.c
@@ -0,0 +1,99 @@
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+
+#include "zmk/trackpad.h"
+#include "zmk/hid.h"
+#include "zmk/endpoints.h"
+// #include "drivers/sensor/gen4.h" //Definitions for custom sensor channels used
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+const struct device *trackpad = DEVICE_DT_GET(DT_INST(0, cirque_gen4));
+
+static zmk_trackpad_finger_contacts_t present_contacts = 0;
+static zmk_trackpad_finger_contacts_t contacts_to_send = 0;
+
+static struct zmk_ptp_finger fingers[CONFIG_ZMK_TRACKPAD_MAX_FINGERS];
+
+#if IS_ENABLED(CONFIG_ZMK_TRACKPAD_WORK_QUEUE_DEDICATED)
+K_THREAD_STACK_DEFINE(trackpad_work_stack_area, CONFIG_ZMK_TRACKPAD_DEDICATED_THREAD_STACK_SIZE);
+static struct k_work_q trackpad_work_q;
+#endif
+
+struct k_work_q *zmk_trackpad_work_q() {
+#if IS_ENABLED(CONFIG_ZMK_TRACKPAD_WORK_QUEUE_DEDICATED)
+    return &trackpad_work_q;
+#else
+    return &k_sys_work_q;
+#endif
+}
+
+static void handle_trackpad(const struct device *dev, const struct sensor_trigger *trig) {
+    int ret = sensor_sample_fetch(dev);
+    if (ret < 0) {
+        LOG_ERR("fetch: %d", ret);
+        return;
+    }
+    LOG_DBG("Trackpad handler trigd %d", 0);
+
+    struct sensor_value contacts, confidence_tip, id, x, y;
+    sensor_channel_get(dev, SENSOR_CHAN_CONTACTS, &contacts);
+    present_contacts = contacts.val1;
+
+    // Get finger coordinates
+    sensor_channel_get(dev, SENSOR_CHAN_X, &x);
+    sensor_channel_get(dev, SENSOR_CHAN_Y, &y);
+    sensor_channel_get(dev, SENSOR_CHAN_CONFIDENCE_TIP, &confidence_tip);
+    sensor_channel_get(dev, SENSOR_CHAN_FINGER, &id);
+
+    // Update array
+    fingers[id.val1].confidence_tip = confidence_tip.val1;
+    fingers[id.val1].contact_id = id.val1;
+    fingers[id.val1].x = x.val1;
+    fingers[id.val1].y = y.val1;
+
+    // Add finger to contacts to send
+    contacts_to_send |= BIT(id.val1);
+}
+
+static void zmk_trackpad_tick(struct k_work *work) {
+    if (contacts_to_send) {
+        for (int i = 0; i < CONFIG_ZMK_TRACKPAD_MAX_FINGERS; i++)
+            if (contacts_to_send & BIT(i)) {
+                LOG_DBG("Writing finger %d report", i);
+                zmk_hid_ptp_set(fingers[i], present_contacts);
+                zmk_endpoints_send_ptp_report();
+                contacts_to_send &= !BIT(i);
+                return;
+            }
+    }
+}
+
+K_WORK_DEFINE(trackpad_work, zmk_trackpad_tick);
+
+static void zmk_trackpad_tick_handler(struct k_timer *timer) {
+    k_work_submit_to_queue(zmk_trackpad_work_q(), &trackpad_work);
+}
+
+K_TIMER_DEFINE(trackpad_tick, zmk_trackpad_tick_handler, NULL);
+
+static int trackpad_init() {
+    struct sensor_trigger trigger = {
+        .type = SENSOR_TRIG_DATA_READY,
+        .chan = SENSOR_CHAN_ALL,
+    };
+    if (sensor_trigger_set(trackpad, &trigger, handle_trackpad) < 0) {
+        LOG_ERR("can't set trigger");
+        return -EIO;
+    };
+    k_timer_start(&trackpad_tick, K_NO_WAIT, K_MSEC(CONFIG_ZMK_TRACKPAD_TICK_DURATION));
+#if IS_ENABLED(CONFIG_ZMK_TRACKPAD_WORK_QUEUE_DEDICATED)
+    k_work_queue_start(&trackpad_work_q, trackpad_work_stack_area,
+                       K_THREAD_STACK_SIZEOF(trackpad_work_stack_area),
+                       CONFIG_ZMK_TRACKPAD_DEDICATED_THREAD_PRIORITY, NULL);
+#endif
+    return 0;
+}
+
+SYS_INIT(trackpad_init, APPLICATION, CONFIG_ZMK_KSCAN_INIT_PRIORITY);
