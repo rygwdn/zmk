2e18c9ccd3d7e7200baf60983d2938d943e27557
ReFil <31960031+ReFil@users.noreply.github.com>
2023-11-07 10:11:59 +0000

    Make Extended NKRO configurable

--- app/include/zmk/hid.h
+++ app/include/zmk/hid.h
@@ -13,7 +13,11 @@
 #include <dt-bindings/zmk/hid_usage.h>
 #include <dt-bindings/zmk/hid_usage_pages.h>
 
+#if IS_ENABLED(CONFIG_ZMK_HID_KEYBOARD_EXTENDED_REPORT)
 #define ZMK_HID_KEYBOARD_NKRO_MAX_USAGE HID_USAGE_KEY_KEYBOARD_LANG8
+#else
+#define ZMK_HID_KEYBOARD_NKRO_MAX_USAGE HID_USAGE_KEY_KEYPAD_EQUAL
+#endif
 
 #define COLLECTION_REPORT 0x03
 
